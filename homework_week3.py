bb619ac37deac8d5a024dbca9fc0d146075c9d9f
entertaein <taeinnis@naver.com>
2020-07-30 22:26:57 +0900

    3주차 숙제
    3주차 숙제_달달한 맛

--- homework_week3.py
+++ homework_week3.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+# 3주차 숙제(달달한 맛)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
